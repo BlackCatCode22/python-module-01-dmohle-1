8a9de6471f82d95c11d4db6948f4b4f9934bb441
dmohle <10038913+dmohle@users.noreply.github.com>
2025-08-08 16:03:42 -0700

    Add files via upload

---
+++ python_test01.py
@@ -0,0 +1,15 @@
+# python_test01.py
+# dH 8/8/25
+
+print("Welcome to python_test01.py")
+
+num1 = 2
+num2 = 4
+sumOfNums = num1 + num2
+print("")
+print("The sum of " + str(num1) + " and " + str(num2) + " is: " + str(sumOfNums))
+print()
+print()
+print("end of python_test01.py")
+
+ 
